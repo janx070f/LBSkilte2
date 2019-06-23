0f50145c208baa58b7bf11a20cc50cde162790f4
CotecK <45693819+CotecK@users.noreply.github.com>
2019-06-24 01:23:53 +0200

    slider
    jeg har slettet den plankede slider og indsat min egen nye

--- assets/js/Script1.js
+++ assets/js/Script1.js
@@ -15,95 +15,30 @@
     mobileNav.addEventListener('click', closeMobile); //when click use function closeMobile
 
 //mikkel forside starter 
-var slideIndex,slides,dots,captionText;
-function initGallery(){
-    slideIndex = 0;
-    slides=document.getElementsByClassName("imageHolder");
-    slides[slideIndex].style.opacity=1;
+let billedIndex = 1;
+visBilleder(billedIndex);
 
-    captionText=document.querySelector(".captionTextHolder .captionText");
-    captionText.innerText=slides[slideIndex].querySelector(".captionText").innerText;
-
-    //disable nextPrevBtn if slide count is one
-    if(slides.length<2){
-        var nextPrevBtns=document.querySelector(".leftArrow,.rightArrow");
-        nextPrevBtns.style.display="none";
-        for (i = 0; i < nextPrevBtn.length; i++) {
-            nextPrevBtn[i].style.display="none";
-        }
-    }
-
-    //add dots
-    dots=[];
-    var dotsContainer=document.getElementById("dotsContainer"),i;
-    for (i = 0; i < slides.length; i++) {
-        var dot=document.createElement("span");
-        dot.classList.add("dots");
-        dotsContainer.append(dot);
-        dot.setAttribute("onclick","moveSlide("+i+")");
-        dots.push(dot);
-    }
-    dots[slideIndex].classList.add("active");
+function nytBilled(x) {
+    visBilleder(billedIndex += x);
 }
-initGallery();
-function plusSlides(n) {
-    moveSlide(slideIndex+n);
+function vistbilled(y) {
+    visBilleder(billedIndex = y);
 }
-function moveSlide(n){
-    var i;
-    var current,next;
-    var moveSlideAnimClass={
-          forCurrent:"",
-          forNext:""
-    };
-    var slideTextAnimClass;
-    if(n>slideIndex) {
-        if(n >= slides.length){n=0;}
-        moveSlideAnimClass.forCurrent="moveLeftCurrentSlide";
-        moveSlideAnimClass.forNext="moveLeftNextSlide";
-        slideTextAnimClass="slideTextFromTop";
-    }else if(n<slideIndex){
-        if(n<0){n=slides.length-1;}
-        moveSlideAnimClass.forCurrent="moveRightCurrentSlide";
-        moveSlideAnimClass.forNext="moveRightPrevSlide";
-        slideTextAnimClass="slideTextFromBottom";
+function visBilleder(z) {
+    let i;
+    let billeder = document.getElementsByClassName("billeder");
+    let cirkler = document.getElementsByClassName("cirkler");
+    if (z > billeder.length) { billedIndex = 1 }
+    if (z < 1) { billedIndex = billeder.length }
+    for (i = 0; i < billeder.length; i++) {
+        billeder[i].style.display = "none";
     }
-
-    if(n!=slideIndex){
-        next = slides[n];
-        current=slides[slideIndex];
-        for (i = 0; i < slides.length; i++) {
-            slides[i].className = "imageHolder";
-            slides[i].style.opacity=0;
-            dots[i].classList.remove("active");
-        }
-        current.classList.add(moveSlideAnimClass.forCurrent);
-        next.classList.add(moveSlideAnimClass.forNext);
-        dots[n].classList.add("active");
-        slideIndex=n;
-        captionText.style.display="none";
-        captionText.className="captionText "+slideTextAnimClass;
-        captionText.innerText=slides[n].querySelector(".captionText").innerText;
-        captionText.style.display="block";
+    for (i = 0; i < cirkler.length; i++) {
+        cirkler[i].className = cirkler[i].className.replace(" active", "");
     }
-
+    if (billedIndex > billeder.length) { billedIndex = 1 }
+    billeder[billedIndex - 1].style.display = "block";
+    cirkler[billedIndex - 1].className += " active";
 }
-var timer=null;
-function setTimer(){
-    timer=setInterval(function () {
-        plusSlides(1) ;
-    },3000);
-}
-setTimer();
-function playPauseSlides() {
-    var playPauseBtn=document.getElementById("playPause");
-    if(timer==null){
-        setTimer();
-        playPauseBtn.style.backgroundPositionY="0px"
-    }else{
-        clearInterval(timer);
-        timer=null;
-        playPauseBtn.style.backgroundPositionY="-33px"
-    }
-}
+setInterval(nytBilled, 5000, 1);
 //mikkel forside slut 
